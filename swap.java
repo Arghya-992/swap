9ce5da04bea82d2189267686a4862408f5802b7d
Arghya-992 <76256630+Arghya-992@users.noreply.github.com>
2022-07-05 22:30:06 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class swap {  
+    public static void main(String[] args) {  
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the first nos");
+        int a = sc.nextInt();
+        System.out.println("Enter the second nos");
+        int b = sc.nextInt();
+
+        /*int swapper = a;
+        a = b;
+        b = swapper;
+        */
+        /*a = a + b;
+        b = a - b;
+        a = a - b;
+        */
+        /*  a = a*b;
+        b = a/b;
+        a = a/b;
+        */
+
+        
+        
+        System.out.println("After swapping: ");
+        System.out.println("The first number = " + a);
+        System.out.println("The second number = " + b);
+    }
+}
